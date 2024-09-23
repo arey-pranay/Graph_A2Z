ec5e4721c69d8e811efb238294600aa23ac45043
Pranay Parikh <86497126+arey-pranay@users.noreply.github.com>
2024-09-23 18:50:38 +0530

    Update 01. NoOfProvinces(NoOfComponents)-#547.java

--- 02. Problems On BFS-DFS/01. NoOfProvinces(NoOfComponents)-#547.java
+++ 02. Problems On BFS-DFS/01. NoOfProvinces(NoOfComponents)-#547.java
@@ -31,46 +31,69 @@
 //     }
 // }
 
+// class Solution {
+//     public int findCircleNum(int[][] isConnected) {
+//         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+//         int V = isConnected.length;
+//         for(int i=0;i<V;i++) adj.add(new ArrayList<>());
+//         for(int i=0;i<V;i++){
+//             for(int j=0;j<V;j++){
+//                 if(isConnected[i][j]==1){
+//                     adj.get(i).add(j);
+//                     // adj.get(b).add(a);
+//                 }
+//             }
+//         }
+//         System.out.println(adj);
+//         boolean[] visited = new boolean[V];
+//         ArrayList<Integer> ans = new ArrayList<>();
+//         int count = 1;
+//         bfs(adj, V, ans, 0, visited);
+//         while(ans.size() != V){
+//             for(int i=0;i<V;i++){
+//                 if(visited[i]==false){
+//                     count++;
+//                     bfs(adj, V, ans, i, visited);
+//                 }
+//             }
+//             System.out.println(count);
+//         }
+//         return count;
+//     }
+//     public void bfs(ArrayList<ArrayList<Integer>> adj, int V, ArrayList<Integer> ans, int start, boolean[] visited){
+//         Queue<Integer> q = new LinkedList<>();
+//         q.add(0);
+//         while(!q.isEmpty()){
+//             int curr = q.remove();
+//             ans.add(curr);
+//             for(int i : adj.get(curr)){
+//                 if(visited[i]) continue;
+//                 visited[i] = true;
+//                 q.add(i);
+//             }
+//         }
+//     }
+// }
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        int V = isConnected.length;
-        for(int i=0;i<V;i++) adj.add(new ArrayList<>());
-        for(int i=0;i<V;i++){
-            for(int j=0;j<V;j++){
-                if(isConnected[i][j]==1){
-                    adj.get(i).add(j);
-                    // adj.get(b).add(a);
-                }
+        int province = 0;
+        boolean[] visited = new boolean[isConnected.length];
+        for(int i=0; i<visited.length;i++){
+                // unvisited nodes pe dfs call krdo
+            if(!visited[i]){
+                dfs(i, isConnected, visited);
+                province++;
             }
         }
-        System.out.println(adj);
-        boolean[] visited = new boolean[V];
-        ArrayList<Integer> ans = new ArrayList<>();
-        int count = 1;
-        bfs(adj, V, ans, 0, visited);
-        while(ans.size() != V){
-            for(int i=0;i<V;i++){
-                if(visited[i]==false){
-                    count++;
-                    bfs(adj, V, ans, i, visited);
-                }
-            }
-            System.out.println(count);
-        }
-        return count;
+        return province;
     }
-    public void bfs(ArrayList<ArrayList<Integer>> adj, int V, ArrayList<Integer> ans, int start, boolean[] visited){
-        Queue<Integer> q = new LinkedList<>();
-        q.add(0);
-        while(!q.isEmpty()){
-            int curr = q.remove();
-            ans.add(curr);
-            for(int i : adj.get(curr)){
-                if(visited[i]) continue;
-                visited[i] = true;
-                q.add(i);
-            }
+    public void dfs(int current, int[][] graph, boolean[] visited){
+        visited[current] = true;
+        for(int i=0; i<graph[current].length;i++){
+                // current ke univisited neighbours pe dfs call krlo
+            if(!visited[i] && graph[current][i] == 1)
+                dfs(i, graph, visited);
         }
     }
 }
